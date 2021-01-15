7701bcf472d2bd271c21c1d948ad001307523de6
Jefreesujit <e.jefree110@gmail.com>
2021-01-15 13:24:10 +0530

    Base code to get the albion bot working

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+require('dotenv').config();
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	console.log(message.content);
+  const { prefix } = config;
+  console.log(message);
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('Pong.');
+	} else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop.');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
+});
+
+client.login(process.env.CLIENT_TOKEN);
