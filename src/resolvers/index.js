2789bf038357dc9106fa8a47138bd7c5767ee68d
Jefreesujit <e.jefree110@gmail.com>
2021-01-15 21:16:46 +0530

    Update resolvers with additional config and handler

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,9 +1,11 @@
 const common = require('./common');
 const guild = require('./guild');
 const player = require('./player');
+const help = require('./help');
 
 module.exports = {
   player,
   guild,
+  help,
   ...common
 };
