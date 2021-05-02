dbac6ee287b6a201f9aa0149fc3801fae395d237
Jefreesujit <e.jefree110@gmail.com>
2021-05-02 13:45:04 +0530

    Add base services for bot api

---
+++ src/services/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const API = require('albion-api');
+
+const getPlayerInfo = (playerName) => {
+  // search for that player
+  // fetch player info
+  return;
+}
+
+const getPlayerInfo = (playerName) => {
+  // search for that player
+  // fetch player info
+  return;
+}
